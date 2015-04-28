2182a6991301d5c2c41d01c0426533a8d2b450be
Brent Enriquez <benriquez0012@gmail.com>
2015-04-27 22:22:53 -0600

    Added real time updates on alerts page

--- public/javascripts/alerts.js
+++ public/javascripts/alerts.js
@@ -10,8 +10,20 @@ $(document).ready(function() {
 		$('#noAlert').remove();
 		createAlertsTable();	
 	}
-	
-})
+
+	socket = io.connect('http://localhost');
+	socket.emit('register', {id : userData.deviceNumber })
+	socket.on('alert', function(alert) {
+		// console.log('inside alert socket');
+		// console.log(alert);
+		addAlertRow(alert);
+	});
+	socket.on('alert-update', function(alert) {
+		// console.log('inside alert-update socket');
+		// console.log(alert);
+		$('#' + alert._id).html(alert.status);
+	})
+});
 
 // table.ui.table
 // 	thead
@@ -28,6 +40,11 @@ $(document).ready(function() {
 // 			td MOTION, TEMPERATURE
 // 			td A42155
 // 			td Alert code sent to +8037955532
+
+function addAlertRow(alert) {
+	$('#alertsBody').prepend(createAlertRow(alert));
+}
+
 function createAlertsTable() {
 	// Create headers
 	var table = document.createElement('table');
@@ -42,7 +59,7 @@ function createAlertsTable() {
 	$(thead).append(headers);
 	$(table).append(thead);
 	var tbody = document.createElement('tbody');
-
+	$(tbody).attr('id', 'alertsBody');
 	for(var k = 0; k < alertsData.length; k += 1) {
 		$(tbody).prepend(createAlertRow(alertsData[k]));
 	}
@@ -59,7 +76,10 @@ function createAlertRow(alert) {
 	$(row).append(createRowEntry(date.toLocaleTimeString())) // time
 	$(row).append(createRowEntry(alert.sensors.join(', ').toUpperCase())); // sensors
 	$(row).append(createRowEntry(alert.token)); // token
-	$(row).append(createRowEntry(alert.status)); // status
+	// Add id to status for update events
+	var status = createRowEntry(alert.status);
+	$(status).attr('id', alert._id);
+	$(row).append(status); // status
 	return row;
 }
 
